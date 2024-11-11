1ffa76794410248f6b18578ba472a981d91df070
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2024-11-11 09:01:10 +0700

    Update ClassInstanceActivity.java

--- app/src/main/java/com/gohool/firstlook/yogaapp/Activity/ClassInstanceActivity.java
+++ app/src/main/java/com/gohool/firstlook/yogaapp/Activity/ClassInstanceActivity.java
@@ -173,7 +173,6 @@ public class ClassInstanceActivity extends AppCompatActivity {
                     saveClassToDB(v, course_id);
                     Toast.makeText(ClassInstanceActivity.this, "Saved Class to DB!", Toast.LENGTH_SHORT).show();
 //                    Toast.makeText(ClassInstanceActivity.this, "The date matches the day of the week!", Toast.LENGTH_SHORT).show();
-//                    Log.d("Hello", "hello");
                 } else {
                     Toast.makeText(ClassInstanceActivity.this, "Sorry, Check the Information Class", Toast.LENGTH_SHORT).show();
 //                    Toast.makeText(ClassInstanceActivity.this, "The date does not match the day of the week!", Toast.LENGTH_SHORT).show();
